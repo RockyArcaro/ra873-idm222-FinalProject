f3d5712fe8fabc00590a986b709b1f1cf8fb4c2b
RockyArcaro <114040029+RockyArcaro@users.noreply.github.com>
2023-03-23 19:35:21 -0400

    New Site W/O stuff

---
+++ javascript.js
@@ -0,0 +1,99 @@
+const streatfest = document.getElementById('streat');
+const streattext = document.getElementById('streattext');
+
+streatfest.addEventListener('click', () => {
+    if (streattext.style.display === "none") {
+        streattext.style.display = "block";
+      } else {
+        streattext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const dragshow = document.getElementById('dragshow');
+const dragshowtext = document.getElementById('dragshowtext');
+
+dragshow.addEventListener('click', () => {
+    if (dragshowtext.style.display === "none") {
+        dragshowtext.style.display = "block";
+      } else {
+        dragshowtext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const hacks = document.getElementById('hacks');
+const hackstext = document.getElementById('hackstext');
+
+hacks.addEventListener('click', () => {
+    if (hackstext.style.display === "none") {
+        hackstext.style.display = "block";
+      } else {
+        hackstext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const whitpain = document.getElementById('whitpain');
+const whitpaintext = document.getElementById('whitpaintext');
+
+whitpain.addEventListener('click', () => {
+    if (whitpaintext.style.display === "none") {
+        whitpaintext.style.display = "block";
+      } else {
+        whitpaintext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const fall = document.getElementById('fall');
+const falltext = document.getElementById('falltext');
+
+fall.addEventListener('click', () => {
+    if (falltext.style.display === "none") {
+        falltext.style.display = "block";
+      } else {
+        falltext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const firstbash = document.getElementById('firstbashPoster');
+const firstbashtext = document.getElementById('firstbashtext');
+
+firstbash.addEventListener('click', () => {
+    if (firstbashtext.style.display === "none") {
+        firstbashtext.style.display = "block";
+      } else {
+        firstbashtext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const job = document.getElementById('jobPoster');
+const jobtext = document.getElementById('jobtext');
+
+job.addEventListener('click', () => {
+    if (jobtext.style.display === "none") {
+        jobtext.style.display = "block";
+      } else {
+        jobtext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const reflect = document.getElementById('reflectPoster');
+const reflecttext = document.getElementById('reflecttext');
+
+reflect.addEventListener('click', () => {
+    if (reflecttext.style.display === "none") {
+        reflecttext.style.display = "block";
+      } else {
+        reflecttext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
+const recruitment = document.getElementById('recruitmentPoster');
+const recruitmenttext = document.getElementById('recruitmenttext');
+
+recruitment.addEventListener('click', () => {
+    if (recruitmenttext.style.display === "none") {
+        recruitmenttext.style.display = "block";
+      } else {
+        recruitmenttext.style.display = "none";
+      }
+});
+////////////////////////////////////////////////////////////
